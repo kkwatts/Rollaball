dbf7d686efd2d64f59be6b30aae5dbcf72871a80
kkwatts <kkwatts15@gmail.com>
2025-02-06 22:00:38 -0600

    Displaying Score and Text
    Added a counter that displays the number of PickUps collected on the screen, as well as a "You win" message that displays once all PickUps have been collected

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,21 +1,38 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour {
     private Rigidbody rb;
+
     private float movementX;
     private float movementY;
     public float speed = 0;
+    private int count;
+
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winTextObject.SetActive(false);
     }
 
     void OnMove(InputValue movementValue) {
         Vector2 movementVector = movementValue.Get<Vector2>();
         movementX = movementVector.x;
         movementY = movementVector.y;
+    }
+
+    void SetCountText() {
+        countText.text = "Count: " + count.ToString();
+
+        if (count >= 12) {
+            winTextObject.SetActive(true);
+        }
     }
 
     private void FixedUpdate() {
@@ -26,6 +43,9 @@ public class PlayerController : MonoBehaviour {
     void OnTriggerEnter(Collider other) {
         if (other.gameObject.CompareTag("PickUp")) {
             other.gameObject.SetActive(false);
+
+            count = count + 1;
+            SetCountText();
         }
     }
 }
